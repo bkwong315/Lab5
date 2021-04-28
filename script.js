26752de01ae8eb7ef5c3ff9ed587635a843dc66d
Brian <bkwong@ucsd.edu>
2021-04-28 15:58:25 -0700

    Started onload eventlistener for img

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ const img = new Image(); // used to load image from <input> and draw to canvas
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO
+  let canvas = document.getElementById("user-image");
+  console.log(canvas);
+  let ctx = canvas.getContext("2d");
 
+  if(img.width !== img.height) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  let dimensions = getDimmensions(canvas.width, canvas.height, img.width, img.height);
+
+  ctx.drawImage(img, dimensions['startX'], dimensions['startY'], dimensions['width'], dimensions['height']);
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
+
+img.src = './images/sky.jpg';
 
 /**
  * Takes in the dimensions of the canvas and the new image, then calculates the new
